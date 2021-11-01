8dca3cd8e9bbdfe9d6913902581ef7ae09f9c45f
Nantawat Chairat <nantawat@opsta.co.th>
2021-11-01 15:29:15 +0700

    update app.js dispatch class

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import Transaction from "./transaction.js";
 import express from "express";
+import CreateTransactionWithMemberCard from "./events_handler/create_transaction_with_member_card.js";
 
-// const express = require('express')
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
     let transaction = new Transaction();
-    transaction.dispatch("CreateTransactionWithMemberCard", {
-        data: { member: { id: "001" } },
+    transaction.dispatch(
+        CreateTransactionWithMemberCard,
+        { data: { member: { id: "001" } },
     });
     res.send({ result: "OK" });
 });
--- transaction.js
+++ transaction.js
@@ -1,9 +1,7 @@
 import CreateTransactionWithMemberCard from "./events_handler/create_transaction_with_member_card.js";
 
 export default class Transaction {
-    dispatch(type, data) {
-        if (type == "CreateTransactionWithMemberCard") {
-            let event = new CreateTransactionWithMemberCard(this.transaction, data);
-        }
+    dispatch(event_type, data) {
+        new event_type(data);
     }
 }
